c19ef3966d9d15501f455c5b06faf229d41b412b
Dave Rojas <dave.rojas@ie.edu>
2018-04-27 18:26:45 +0200

    vuex plus axios

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,41 @@
 // /services/index.js
-import socketio from 'feathers-socketio/client'
-import io from 'socket.io-client'
+import axios from 'axios'
+/*import io from 'socket.io-client'*/
 
-const socket = io('http://localhost:3030')
+const _URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const _CITY = 'Madrid'
+const _LANG = 'ES'
+const _APPID = 'aae97860de78632108ecc73036d6d17c'
+const _RESULT = ''
+
 
 // repeat this line for every service in our backend
-export const messageService = app.service('messages')
+export default{
+
+    //METHODS
+    methods{
+        // SEARCH A NEW CITY
+        searchcity(city){
+            this.cityName = city;
+            var ctx = this;
+            ctx.performAJAX();
+        },        
+        // GET WEATHER DATA                     
+        performAJAX(){
+        var ctx = this;
+        
+        axios.get(_URL + 
+                    ctx._CITY + '&units=metric&lang=' + 
+                    ctx._LANG + '&APPID=' + ctx._APPID )
+        .then(function (response) { 
+            ctx._RESULT = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        }
+    }
+}
 
 // in  actions.js, your component and anywhere else you need your services:
 // import messageService from './services';
-
-// or load all services at once: import * as services from './services'
-// and access them as services.messageService etc.
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,22 +1,22 @@
 import * as services from '../services'
 
-export const fetchMessages = function ({dispatch}) {
+export const fetchCities = function ({dispatch}) {
   // Call the messages service on the server via websocket
-  services.messageService.find({}).then(messages => {
-    dispatch('FETCH_MESSAGES', messages.data)
+  services.messageService.find({}).then(cities => {
+    dispatch('FETCH_CITIES', cities.data)
   })
 }
 
-export const addMessage = function ({dispatch}) {
+export const addCity = function ({dispatch}) {
   // A new message has been created on the server, so dispatch a mutation to update our state/view
-  services.messageService.on('created', message => {
-    dispatch('ADD_MESSAGE', message)
+  services.cityService.on('created', city => {
+    dispatch('ADD_CITY', city)
   })
 }
 
 export const removeMessage = function ({dispatch}) {
   // A message has been removed from the server, so dispatch a mutation to update our state/view
-  services.messageService.on('removed', message => {
-    dispatch('REMOVE_MESSAGE', message)
+  services.cityService.on('removed', city => {
+    dispatch('REMOVE_CITY', city)
   })
 }
